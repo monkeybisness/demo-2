c61cc908ed573226dde899fac0a4b26ced67af92
monkeybisness <User@DESKTOP-MIVQ3QL>
2024-02-19 02:55:48 +0500

    Second commit

--- demo-2/src/main/java/com/example/demo/model/Post.java
+++ demo-2/src/main/java/com/example/demo/model/Post.java
@@ -1,13 +1,21 @@
 package com.example.demo.model;
 
+import java.sql.Date;
+
 public class Post {
     private String text;
     private Integer likes;
 
-    public Post(String _text){
+    private Date creationDate;
+
+    public Post(String _text, Date _creationDate){
+        creationDate = _creationDate;
         text = _text;
     }
 
+    public Date getCreationDate(){
+        return creationDate;
+    }
     public String getText(){
         return text;
     }
--- demo-2/src/main/java/com/example/demo/service/PostService.java
+++ demo-2/src/main/java/com/example/demo/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,9 +11,9 @@ import java.util.List;
 public class PostService {
     public List<Post> listAllPosts(){
         List<Post> posts = new ArrayList<>();
-        posts.add(new Post("Hello world!!!"));
-        posts.add(new Post("!!!"));
-        posts.add(new Post("!!!"));
+        posts.add(new Post("Hello world!!!", new Date(0,0,0)));
+        posts.add(new Post("!!!", new Date(0,0,0)));
+        posts.add(new Post("!!!", new Date(0,0,0)));
         return posts;
     }
 }
