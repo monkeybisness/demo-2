4caa4987976e2d86e31991d5c6bcabf7d166dd91
monkeybisness <User@DESKTOP-MIVQ3QL>
2024-02-19 03:00:28 +0500

    Third commit

--- demo-2/src/main/java/com/example/demo/service/PostService.java
+++ demo-2/src/main/java/com/example/demo/service/PostService.java
@@ -12,7 +12,7 @@ public class PostService {
     public List<Post> listAllPosts(){
         List<Post> posts = new ArrayList<>();
         posts.add(new Post("Hello world!!!", new Date(0,0,0)));
-        posts.add(new Post("!!!", new Date(0,0,0)));
+        posts.add(new Post("!!!", new Date(200,0,0)));
         posts.add(new Post("!!!", new Date(0,0,0)));
         return posts;
     }
